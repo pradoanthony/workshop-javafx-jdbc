424be41251c6ce3445a63fec315bdfe2b30dcff3
pradoanthony <anthonyqueiroz047@gmail.com>
2024-10-22 20:25:47 -0300

    department combo box

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -109,6 +109,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO exception", null, e.getMessage(), AlertType.ERROR);
 		}
 	}
